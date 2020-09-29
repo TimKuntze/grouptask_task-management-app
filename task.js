094154ac113b9593bcd0b14bb60e9794676dfee9
Hồng-Hạnh, Chu <67581292+Hanmuni@users.noreply.github.com>
2020-09-29 21:54:36 +0200

    Add files via upload
    Built on Atila's Design I added the Backlog Section in order to see which person is responsible for which task.

---
+++ task.js
@@ -0,0 +1,59 @@
+let alltasks = [];
+let id = 1;
+
+let titleValue;
+let categoryValue;
+let descriptionValue;
+let dateValue;
+let urgencyValue;
+let task;
+
+function setTodaysDate() {
+    let today = new Date().toISOString().substr(0, 10);
+    document.querySelector("#dateInput").value = today;
+}
+
+function createTaskJSON() {
+    gatherTasksData();
+    assignJSONToVariable();
+
+    id++
+    alltasks.push(task);
+
+    /* Only for information */
+    console.log(alltasks)
+
+    resetPage();
+}
+
+function gatherTasksData() {
+    titleValue = document.getElementById('titleInput').value;
+    categoryValue = document.getElementById('categorySelector').value;
+    descriptionValue = document.getElementById('taskDescription').value;
+    urgencyValue = document.getElementById('urgencySelector').value;
+    dateValue = document.querySelector("#dateInput").value;
+}
+
+function assignJSONToVariable() {
+    task = {
+        "title": `${titleValue}`,
+        "category": `${categoryValue}`,
+        "description": `${descriptionValue}`,
+        "date": `${dateValue}`,
+        "urgency": `${urgencyValue}`,
+        "status": `to do`,
+        /* Next it will be created a databank of people that can be assigned for a task */
+        /* Name, e-mail and avatar's pic */
+        "assigned": `responsablesArray`,
+        "id": `${id}`
+    };
+}
+
+function resetPage() {
+    document.getElementById('titleInput').value = ``;
+    document.getElementById('categorySelector').value = `Select a category:`;
+    document.getElementById('taskDescription').value = ``
+    document.getElementById('urgencySelector').value = `Select the urgency:`
+
+    setTodaysDate();
+}
